d1bb2e2d7570f3739d4655909480b814c14f6d5a
ETU1249 <89473718+sedraRabemananjara@users.noreply.github.com>
2023-07-24 19:18:35 +0200

    lister Site Touristique

--- back/index.js
+++ back/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Successful response.');
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+app.set('view engine', 'ejs');
+
+
+app.get('/sites',function(req,res) {
+	client.connect(uri,function (err,db) {
+		if(err) throw err;
+		var dbo=db.db("sitetouristique");
+		dbo.collection("site").find({}).toArray(function (err,ress) {
+			db.close();
+			res.send(ress);
+		})
+	})
+});
+
+app.listen(port, () => {
+    console.log(`Serveur Express en cours d'exécution sur le port ${port}.`);
   });
   
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));

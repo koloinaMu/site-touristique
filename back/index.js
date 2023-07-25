a07ab49a294aa2f043405d5ab60775b39d5b763c
ETU1249 <89473718+sedraRabemananjara@users.noreply.github.com>
2023-07-25 20:10:57 +0200

    final version about Liste site Back_End + details

--- back/index.js
+++ back/index.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+var express=require("express");
+var app=express();
+var cors=require('cors');
+app.use(cors());
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+const uri =
+"mongodb://127.0.0.1:27017/touriste";
+var md5=require("md5");
 
-const app = express();
-const port = 3000;
 
-const uri = "mongodb://127.0.0.1:27017";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-app.set('view engine', 'ejs');
+var jsonParser = bodyParser.json();
+var MongoClient=mongo.MongoClient;
+ 
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 
 
 app.get('/sites',function(req,res) {
-	client.connect(uri,function (err,db) {
+	MongoClient.connect(uri,function (err,db) {
 		if(err) throw err;
 		var dbo=db.db("sitetouristique");
 		dbo.collection("site").find({}).toArray(function (err,ress) {
@@ -21,7 +27,26 @@ app.get('/sites',function(req,res) {
 	})
 });
 
-app.listen(port, () => {
-    console.log(`Serveur Express en cours d'exécution sur le port ${port}.`);
-  });
-  
+
+app.get('/details_site/:id',function(req,res) {
+	var id=req.params.id;
+	MongoClient.connect(uri,function (err,db) {
+		if(err) throw err;
+		
+		var myquery = { _id: new mongo.ObjectId(id) };
+		var dbo=db.db("sitetouristique");
+	
+		dbo.collection("site").find(myquery).toArray(function (err,ress) {
+			console.log(query);
+			db.close();			
+			res.send(ress);
+		});
+	})
+});
+
+
+app.listen(3000,function () {
+	console.log("start app");
+});
+
+

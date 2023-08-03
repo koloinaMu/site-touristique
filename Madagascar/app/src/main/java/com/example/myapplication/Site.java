a256f2deb66084465513efba0f4d94359daf052f
ETU1249 <89473718+sedraRabemananjara@users.noreply.github.com>
2023-08-03 18:14:14 +0200

    listeView Android

---
+++ Madagascar/app/src/main/java/com/example/myapplication/Site.java
@@ -0,0 +1,104 @@
+package com.example.myapplication;
+
+import org.w3c.dom.Text;
+
+class Site {
+   private int id;
+   private String nom;
+   private String description;
+   private String region;
+   private int idMedia;
+   private String urlMedia;
+   private String descriptionMedia;
+   private String typeMedia;
+
+   public int getId() {
+      return id;
+   }
+
+   public void setId(int id) {
+      this.id = id;
+   }
+
+   public String getNom() {
+      return nom;
+   }
+
+   public void setNom(String nom) {
+      this.nom = nom;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+
+   public String getRegion() {
+      return region;
+   }
+
+   public void setRegion(String region) {
+      this.region = region;
+   }
+
+   public int getIdMedia() {
+      return idMedia;
+   }
+
+   public void setIdMedia(int idMedia) {
+      this.idMedia = idMedia;
+   }
+
+   public String getUrlMedia() {
+      return urlMedia;
+   }
+
+   public void setUrlMedia(String urlMedia) {
+      this.urlMedia = urlMedia;
+   }
+
+   public String getDescriptionMedia() {
+      return descriptionMedia;
+   }
+
+   public void setDescriptionMedia(String descriptionMedia) {
+      this.descriptionMedia = descriptionMedia;
+   }
+
+   public String getTypeMedia() {
+      return typeMedia;
+   }
+
+   public void setTypeMedia(String typeMedia) {
+      this.typeMedia = typeMedia;
+   }
+
+   public Site(int id, String nom, String description, String region, int idMedia, String urlMedia, String descriptionMedia, String typeMedia) {
+      this.id = id;
+      this.nom = nom;
+      this.description = description;
+      this.region = region;
+      this.idMedia = idMedia;
+      this.urlMedia = urlMedia;
+      this.descriptionMedia = descriptionMedia;
+      this.typeMedia = typeMedia;
+   }
+
+   public Site(String nom, String description, String region, int idMedia, String urlMedia, String descriptionMedia, String typeMedia) {
+      this.nom = nom;
+      this.description = description;
+      this.region = region;
+      this.idMedia = idMedia;
+      this.urlMedia = urlMedia;
+      this.descriptionMedia = descriptionMedia;
+      this.typeMedia = typeMedia;
+   }
+
+   public Site(String nom, String description) {
+      this.nom = nom;
+      this.description = description;
+   }
+}
--- Madagascar/app/src/main/java/com/example/myapplication/SiteActivity.java
+++ Madagascar/app/src/main/java/com/example/myapplication/SiteActivity.java
@@ -9,20 +9,31 @@ import okhttp3.Response;
 
 import android.os.Bundle;
 import android.widget.TextView;
-
+import java.util.*;
 import java.io.IOException;
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 public class SiteActivity extends AppCompatActivity {
-    private TextView textview;
+    private ListView listView;
+    private List<Site> siteDataList;
+    private ArrayAdapter<Site> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_site);
-        textview= findViewById(R.id.text_view);
 
-        OkHttpClient client= new OkHttpClient();
-        String url= "http://10.0.2.2:3000/sites";
-        Request request= new Request.Builder()
+        listView = findViewById(R.id.listeView);
+        siteDataList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, R.layout.list_item_layout, siteDataList);
+        listView.setAdapter(adapter);
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "http://10.0.2.2:3000/sites";
+        Request request = new Request.Builder()
                 .url(url)
                 .build();
         client.newCall(request).enqueue(new Callback() {
@@ -33,14 +44,27 @@ public class SiteActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                if(response.isSuccessful()){
+                if (response.isSuccessful()) {
                     String myresponse = response.body().string();
-                    SiteActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            textview.setText(myresponse);
+                    try {
+                        JSONArray jsonArray = new JSONArray(myresponse);
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            String siteName = jsonObject.getString("nom");
+                            String siteDescription = jsonObject.getString("description");
+
+                            Site siteData = new Site(siteName, siteDescription);
+                            siteDataList.add(siteData);
                         }
-                    });
+                        SiteActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                adapter.notifyDataSetChanged();
+                            }
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
